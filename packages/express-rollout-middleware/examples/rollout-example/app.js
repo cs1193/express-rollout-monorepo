16639a5215b36d199bf3f5c045e92c0e183710bc
Chandresh Rajkumar Manonmani <cs1193@gmail.com>
2020-06-14 19:45:17 +0530

    fix: dev: master: rollout-example middleware

--- packages/express-rollout-middleware/examples/rollout-example/app.js
+++ packages/express-rollout-middleware/examples/rollout-example/app.js
@@ -12,11 +12,27 @@ const rollout = expressRollout.initialize({
   featureMetaFile: path.resolve(__dirname, './feature-meta.yml')
 });
 
-app.use(cors());
-app.use(helmet());
-app.use(rollout.middleware);
+const HOST = process.env.HOST || '0.0.0.0';
+const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+///////////////// rollout middleware //////////////////
+app.use(rollout.middleware);
+///////////////// rollout middleware //////////////////
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'rollout-example'
+  })
+});
 
 const server = http.createServer(app);
 
